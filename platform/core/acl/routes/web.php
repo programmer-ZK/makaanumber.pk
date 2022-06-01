d951c4e510a597c35915f8e3b90cff33f01d15c7
AHMED <programmer@octopusdigitalnetwork.com>
2022-06-01 17:10:43 +0500

    /

--- platform/core/acl/routes/web.php
+++ platform/core/acl/routes/web.php
@@ -20,8 +20,8 @@ Route::group(['namespace' => 'Botble\ACL\Http\Controllers', 'middleware' => ['we
       Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])
         ->name('access.password.email');
 
-        // Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])
-        //   ->name('access.password.reset');
+      Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])
+        ->name('access.password.reset');
       Route::post('password/reset', [ResetPasswordController::class, 'reset'])
         ->name('access.password.reset.post');
     });
