c93be91639f1e4e8551fa8dd1bbdd7642b0fae5b
AHMED <programmer@octopusdigitalnetwork.com>
2022-06-03 18:04:48 +0500

    .

--- platform/themes/flex-home/partials/header.blade.php
+++ platform/themes/flex-home/partials/header.blade.php
@@ -310,8 +310,10 @@ if (Auth::user()) {
 
           <li class="nav-item mt-2 ml-3 active" style="border:none !important;">
             <span class="mr-5">
-              <img class="flag1 mr-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Flag_of_Pakistan.svg/320px-Flag_of_Pakistan.svg.png" />
-              <img class="flag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/255px-Flag_of_the_United_Arab_Emirates.svg.png" />
+                <img class="flag1 mr-1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Flag_of_Pakistan.svg/320px-Flag_of_Pakistan.svg.png" />
+              <a href="https://www.ae.makanumber.com">
+                <img class="flag" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/255px-Flag_of_the_United_Arab_Emirates.svg.png" />
+              </a>
             </span>
 
           </li>
