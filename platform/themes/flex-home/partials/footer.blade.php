1cf5f8066e0e176ac1b67c43f19c5365070b912a
AHMED <programmer@octopusdigitalnetwork.com>
2022-06-03 16:18:46 +0500

    .

--- platform/themes/flex-home/partials/footer.blade.php
+++ platform/themes/flex-home/partials/footer.blade.php
@@ -116,7 +116,49 @@
 </div>
 
 
-
+{{-- <footer>
+    <br>
+    <div class="container-fluid w90">
+        <div class="row">
+            <div class="col-sm-3">
+                @if (theme_option('logo'))
+                <p>
+                    <a href="{{ route('public.single') }}">
+<img src="{{ RvMedia::getImageUrl(theme_option('logo'))  }}" style="max-height: 38px" alt="{{ theme_option('site_name') }}">
+</a>
+</p>
+@endif
+@if (theme_option('address'))
+<p><i class="fas fa-map-marker-alt"></i> &nbsp;{{ theme_option('address') }}</p>
+@endif
+@if (theme_option('hotline'))
+<p><i class="fas fa-phone-square"></i> {{ __('Hotline') }}: &nbsp;<a href="tel:{{ theme_option('hotline') }}">{{ theme_option('hotline') }}</a></p>
+@endif
+@if (theme_option('email'))
+<p><i class="fas fa-envelope"></i> {{ __('Email') }}: &nbsp;<a href="mailto:{{ theme_option('email') }}">{{ theme_option('email') }}</a></p>
+@endif
+</div>
+<div class="col-sm-9 padtop10">
+  <div class="row">
+    {!! dynamic_sidebar('footer_sidebar') !!}
+  </div>
+</div>
+</div>
+<div class="row">
+  <div class="col-12">
+    {!! Theme::partial('language-switcher') !!}
+  </div>
+</div>
+<div class="copyright">
+  <div class="col-sm-12">
+    <p class="text-center">
+      {!! clean(theme_option('copyright')) !!}
+    </p>
+  </div>
+</div>
+</div>
+</footer> --}}
+<!--FOOTER-->
 
 <style>
   .green1 {
@@ -165,16 +207,6 @@
 </style>
 
 <script>
-  (function($) {
-    $(window).load(function() {
-      $('.loader').fadeOut(500, function() {
-        $('.loader').remove();
-      });
-    });
-  });
-
-  console.log("footer script working!");
-
   window.trans = {
     "Price": "{{ __('Price') }}",
     "Number of rooms": "{{ __('Number of rooms') }}",
@@ -215,6 +247,16 @@
     console.log(new Error().stack);
     old.apply(window, arguments);
   };
+
+  $(document).ready(function() {
+    window.onload = function() {
+      $('.loader').fadeOut(500, function() {
+        $('.loader').remove();
+      });
+    }
+  });
+
+  console.log("footer script working!");
 </script>
 {!! Theme::footer() !!}
 </body>
