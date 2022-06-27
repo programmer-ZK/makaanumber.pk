53f09f4b3e6e4aabccfd1a14604a95b825248cf9
AHMED <programmer@octopusdigitalnetwork.com>
2022-06-27 16:17:19 +0500

    autoplay

--- platform/themes/flex-home/views/real-estate/property.blade.php
+++ platform/themes/flex-home/views/real-estate/property.blade.php
@@ -255,7 +255,7 @@ if ($property->type == 'sale') {
       @foreach($props as $prop)
       @if ($prop->video)
       <h5 class="headifhouse">Property Video</h5>
-      <video style="max-width:100%;" height="365" controls autoplay muted>
+      <video style="max-width:100%;" height="365" controls  muted>
         <source src="{{asset('public/storage/properties/'.$prop->video)}}" type="video/mp4">
       </video>
       @else
